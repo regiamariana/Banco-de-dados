15ee81a9bf5c8613f6992cd9b72df459a89ab535
Mariana Régia Freitas Guirado <47425337800@info.local>
2019-05-09 17:25:07 -0300

    oi

---
+++ FinancaDeMesa/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using FinancaDeMesa.Util;
+
+namespace FinancaDeMesa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool querSair = true;
+            do
+            {
+                MenuUtil.MenuDeslogado();
+                
+            } while (true);
+            Console.WriteLine("=============================================");
+            System.Console.WriteLine("");
+        }
+    }
+}
---
+++ FinancaDeMesa/Util/MenuUtil.cs
@@ -0,0 +1,29 @@
+namespace FinancaDeMesa.Util
+{
+    public class MenuUtil
+    {
+        public static void MenuDeslogado (){
+            System.Console.WriteLine("=============================================");
+            System.Console.WriteLine("           Seja bem-vindo à MobiTec          ");
+            System.Console.WriteLine("=============================================");
+            System.Console.WriteLine("           Escolha 1 opção:                  ");
+            System.Console.WriteLine("           1 - cadastro de usuário           ");
+            System.Console.WriteLine("           2 - login de usuário              ");
+            System.Console.WriteLine("           0 - sair                          ");
+            System.Console.WriteLine("=============================================");
+        }
+
+        public static void MenuLogado (){
+            System.Console.WriteLine("=============================================");
+            System.Console.WriteLine("           Seja bem-vindo à MobiTec          ");
+            System.Console.WriteLine("=============================================");
+            System.Console.WriteLine("           Parabéns por se logar!            ");
+            System.Console.WriteLine("           1 - cadastrar transações          ");
+            System.Console.WriteLine("           2 - exibir transações totais      ");
+            System.Console.WriteLine("           3 - relatar no Word               ");
+            System.Console.WriteLine("           4 - exportar banco de dados       ");
+            System.Console.WriteLine("           0 - sair                          ");
+            System.Console.WriteLine("=============================================");
+        }
+    }
+}
